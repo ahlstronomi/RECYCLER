7a14db4f378e5efeecd42f5f194cedf636a8b612
Mikael Ahlström <mikael.ahlstrom@cgi.com>
2018-04-27 12:53:28 +0300

    this does not work yet. Need the .mlmodel

---
+++ camera-view/camera-view/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  camera-view
+//
+//  Created by Mikael Ahlström on 26/04/2018.
+//  Copyright © 2018 Mikael Ahlström. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    let mlModel = 
+    let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Label"
+        label.font = label.font.withSize(30)
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCaptureSession()
+        
+        view.addSubview(label)
+        setupLabel()
+    }
+
+    func setupCaptureSession() {
+        let captureSession = AVCaptureSession()
+        
+        // search for available capture devices
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        // setup capture device, add input to our capture session
+        do {
+            if let captureDevice = availableDevices.first {
+                let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+                captureSession.addInput(captureDeviceInput)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // setup output, add output to our capture session
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(captureOutput)
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    // called everytime a frame is captured, Resnet 50 is the ML-model, figure out how to put our own here
+    
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {return}
+        
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let Observation = results.first else { return }
+            
+            DispatchQueue.main.async(execute: {
+                self.label.text = "\(Observation.identifier)"
+            })
+        }
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // executes request
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+
+    
+    func setupLabel() {
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
+}
