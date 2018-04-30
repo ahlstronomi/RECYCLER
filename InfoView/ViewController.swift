e075649d7cfc7859608ac3e304fb6a44ead4cdb1
iosdev <iosdev@mtk-idev-mba-15.local>
2018-04-30 13:50:59 +0300

    30.4 infoView almost ready

--- InfoView/ViewController.swift
+++ InfoView/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
+    var shadowColor: UIColor? = UIColor.black
+    var shadowOffsetWidth: Int = 0
+    var shadowOffsetHeight: Int = 5
+    var shadowOpacity: Float = 0.5
+    
     @IBAction func indexChanged(_ sender: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex{
          
@@ -43,13 +48,17 @@ class ViewController: UIViewController {
         secondView.isHidden = true
         firstView.isHidden = false
         
-        // Rounded corners in info layout
+        // Rounded corners and shadows in info layout
         self.infoView.layer.cornerRadius = 9
         self.infoView.layer.masksToBounds = true
-        self.infoView.layer.borderColor = UIColor.black.cgColor
-        self.infoView.layer.borderWidth = 1
+        self.infoView.layer.shadowColor = shadowColor?.cgColor
+        self.infoView.layer.cornerRadius = 2
+        self.infoView.layer.shadowOffset = CGSize(width: 0.5, height: 0.4)  //Here you control x and y
+        self.infoView.layer.shadowOpacity = 0.5
+        self.infoView.layer.shadowRadius = 5.0 //Here your control your blur
+        self.infoView.layer.masksToBounds =  false
 
-        
+
     }
 
     
